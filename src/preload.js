ebc58ab77ca85b7ea154104609604409381e858b
arakilian0 <arakilian0@gmail.com>
2024-09-13 05:31:10 -0700

    UPDATE src scripts

--- src/preload.js
+++ src/preload.js
@@ -1,3 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-    
-})
+    var cells = 50000;
+    var grid = document.getElementById('grid');
+
+    for(var i = 0; i <= cells; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        grid.appendChild(cell);
+    }
+});
