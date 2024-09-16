c843f7657da1bb753452a9ec9c7b95b571b60c1f
arakilian0 <arakilian0@gmail.com>
2024-09-15 21:51:34 -0700

    UPDATE gridCells.js cellCount

--- src/scripts/gridCells.js
+++ src/scripts/gridCells.js
@@ -1,4 +1,4 @@
-function gridCells (elmnt, cellCount = 6000) {
+function gridCells (elmnt, cellCount) {
     for(var i = 0; i <= cellCount; i++) {
         let cell = document.createElement('div');
         cell.setAttribute('id', i);
