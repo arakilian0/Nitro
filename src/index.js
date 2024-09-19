7a9de20d6b50aa738d74727baaf4699e435cc7cc
arakilian0 <arakilian0@gmail.com>
2024-09-19 13:34:24 -0700

    CREATE css buffer on loading screen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
     var wrapper = document.getElementById('wrapper');
+    var buffer = document.getElementById('buffer-wrapper');
     var grid = document.getElementById('grid');
     var panel = document.getElementById('panel');
     var panelResizerDiv = document.getElementById('panel-resizer');
 
-    wrapper.style.display = 'block';
+    setTimeout(() => {
+        wrapper.style.display = 'block';
+        buffer.remove();
+    }, "3000");
+
     panelResizer(panelResizerDiv, panel);
 
     gridCells(grid, 300000);
@@ -17,7 +22,6 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     
     window.nitro.setStore('project', '123');
-    window.nitro.setStore('zoom', '123');
 
     test1();
 });
