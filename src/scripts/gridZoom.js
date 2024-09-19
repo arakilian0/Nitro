ea366c220bdf76ec3336e7dc527ebb8b02c89b84
arakilian0 <arakilian0@gmail.com>
2024-09-19 13:29:01 -0700

    UPDATE zoom to read store values

--- src/scripts/gridZoom.js
+++ src/scripts/gridZoom.js
@@ -1,5 +1,13 @@
 function gridZoom (elmnt) {
+
+    async function setInitialZoom () {
+        const store = await window.nitro.getStore();
+        elmnt.style.zoom = store.zoom;
+    };
+
     let zoom = 1;
+    setInitialZoom ();
+
     elmnt.addEventListener("wheel", function(e){ 
         if (e.shiftKey) {
             const delta = Math.sign(e.deltaY);
@@ -9,21 +17,17 @@ function gridZoom (elmnt) {
                 if (zoom.toFixed(1) < 1.5) {
                     zoom = zoom + 0.5;
                     elmnt.style.zoom = zoom.toFixed(1);
+                    window.nitro.setStore('zoom', zoom.toFixed(1));
                 }
             } else if (delta === 1) {
                 // Scroll down
                 if (zoom.toFixed(1) > 0.5) {
                     zoom = zoom - 0.5;
                     elmnt.style.zoom = zoom.toFixed(1);
+                    window.nitro.setStore('zoom', zoom.toFixed(1));
                 }
             }
-        } else if (e.ctrlKey) {
-            if (elmnt.style.zoom !== 1) {
-                elmnt.style.zoom = 1;
-            }
         }
-
-        
 
     }, false);
 }
